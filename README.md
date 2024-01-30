125164b3e9ab73084c8abcf56c60e03688c87a3b
nikunj-bhuva <nikunjbhuva121@gmail.com>
2024-01-30 11:35:54 +0530

    love you

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # indicator-project
 ![Alt text](1.png)
+![Alt text](2.png)
+![Alt text](3.png)
--- main.c
+++ main.c
@@ -26,5 +26,19 @@
 
 // 2 QUE //
 
-int arr[]={1,3,5,7,9};
-int *p arr[5];
+int main()
+{
+    // int arr[] = {1, 3, 5, 7, 9};
+    // int *p = &arr;
+    // printf("%u %d\n", p, *p);
+    // printf("%u %d\n", p+1, *(p+1));
+    // printf("%u %d\n", p+2, *(p+2));
+    // printf("%u %d\n", p+3, *(p+3));
+    // printf("%u %d\n", p+4, *(p+4));
+
+    int a=20;
+    int *p=&a;
+    int **p2=&p;
+    printf("%u",p2,**p2);
+    return 0;
+}
